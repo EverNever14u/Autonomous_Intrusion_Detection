3b304b8f5943019d191228527a5eb787b7e1c4b8
EverNever14u <klew200420042004@gmail.com>
2026-03-17 11:46:15 +0100

    chore: init project

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import gymnasium as gym
+import stable_baselines3
+import sklearn
+
+def main():
+    print(f"test")
+
+if __name__ == "__main__":    
+    main()
