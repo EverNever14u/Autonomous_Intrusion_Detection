8205fa987a6334bf0a0051f0f575fa431d0a4a33
EverNever14u <klew200420042004@gmail.com>
2026-04-15 19:07:38 +0200

    feat: add data visualization

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import numpy as np
+import shutil
 import time
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+import os
+
 
 # ANSI color codes
 class Color:
@@ -62,3 +67,67 @@ print("-" * 30)
 print(f"Data processing complete")
 print(f"Training rows: {X_train.shape[0]}")
 print(f"Test rows: {X_test.shape[0]}")
+
+
+
+# CLEANED_DIR = "data/cleaned"
+
+ 
+# start_time = time.time()
+# if os.path.exists(CLEANED_DIR):
+#     shutil.rmtree(CLEANED_DIR)  
+# os.makedirs(CLEANED_DIR)        
+# print(f"Folder {CLEANED_DIR} cleared and recreated {get_duration(start_time)}")
+
+
+# X_train_df = pd.DataFrame(X_train, columns=X.columns)
+# X_test_df = pd.DataFrame(X_test, columns=X.columns)
+
+
+# start_time = time.time()
+# X_train_df.to_csv(f"{CLEANED_DIR}/X_train.csv", index=False)
+# X_test_df.to_csv(f"{CLEANED_DIR}/X_test.csv", index=False)
+# y_train.to_csv(f"{CLEANED_DIR}/y_train.csv", index=False)
+# y_test.to_csv(f"{CLEANED_DIR}/y_test.csv", index=False)
+
+# print(f"All files saved{CLEANED_DIR} {get_duration(start_time)}")
+# print("-" * 30)
+
+print(f"{Color.GREEN}FIN{Color.RESET}")
+
+
+
+#plots
+
+
+plt.figure(figsize=(10, 5)) 
+label_names = le.inverse_transform(y)
+sns.countplot(
+    y=label_names, 
+    order=pd.Series(label_names).value_counts().index, 
+    palette='viridis'
+)
+plt.title('Attacks vs Normal Traffic')
+plt.xlabel('Number of samples')
+plt.ylabel('Traffic type')
+plt.tight_layout()
+
+
+
+feature_idx = 1 
+feature_name = X.columns[feature_idx]
+
+fig2, axes2 = plt.subplots(1, 2, figsize=(14, 5))
+
+sns.histplot(X.iloc[:, feature_idx], bins=50, ax=axes2[0], color='orange', kde=True)
+axes2[0].set_title(f'Before scaling: {feature_name}')
+axes2[0].set_ylabel('Count')
+
+sns.histplot(X_scaled[:, feature_idx], bins=50, ax=axes2[1], color='blue', kde=True)
+axes2[1].set_title(f'After scaling: {feature_name}')
+axes2[1].set_ylabel('Count')
+
+plt.tight_layout()
+plt.show()
+
+
